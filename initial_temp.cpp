111f74623636b8ea1411f2c7b0d6da44ae1ae901
VedeshP <vedeshskp@gmail.com>
2025-03-15 16:31:07 +0530

    changes made to template

--- initial_temp.cpp
+++ initial_temp.cpp
@@ -1,8 +1,16 @@
 #define ll long long
 #define pii pair<int,int>
+#define vi vector<int>
+
+#define F first
+#define S second
+#define PB push_back
+
+#define fr(i, a, b) for (int i = a; i < b; i++)
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
