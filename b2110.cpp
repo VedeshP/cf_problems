db1539b675d749c85a1553fa2e2dffde76f5e6c3
VedeshP <vedeshskp@gmail.com>
2025-05-25 13:32:13 +0530

    solved

--- b2110.cpp
+++ b2110.cpp
@@ -28,56 +28,22 @@ using namespace std;
 
 void solve()
 {
-    // ll n; cin >> n;
     string s; 
     cin >> s;
     ll n = s.size();
-    // stack<char> st;
-    // fr (i, 0, n) cout << s[i];
-    // cout << '\n';
-    // if (s.size() == 0)
-    // {
-    //     cout << "NO" << '\n';
-    //     return;
-    // }
-    ll start = n / 2, end = n / 2 + 1;
-    cout << end << '\n';
-    // ll left = 0, right = 0;
-    while (end < n && start >= 0)
+    ll bal = 0;
+    fr (i, 0, n)
     {
-        if (s[start] == ')' || s[end] == '(')
+        if (i > 0 && i < n - 1 && bal == 0)
         {
             cout << "YES" << '\n';
             return;
         }
-        start--; end++;
+        if (s[i] == '(') bal++;
+        else bal--;
     }
-    cout << "NO" << '\n';
-    // fr (i, 1, n)
-    // {
-    //     if (s[i - 1] == ')' && s[i] == '(')
-    //     {
-    //         cout << "YES" << '\n';
-    //         return;
-    //     }
-    // }
-    // cout << "NO" << '\n';
-    // fr (i, 0, n)
-    // {
-    //     if (s[i] == '(' && (i > n / 2))
-    //     {
-    //         cout << "YES" << '\n';
-    //         return;
-    //     }
-    // }
-    // cout << "NO" << '\n';
-    // fr (i, 0, n)
-    // {
-    //     if (s[i] == '(' && i < n / 2)
-    //     {
 
-    //     }
-    // }
+    cout << "NO" << '\n';
 }
 
 int main(void)
