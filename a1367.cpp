16e6e9fb9a9543435497ded3c44ea31ad5f3d422
VedeshP <vedeshskp@gmail.com>
2025-06-20 09:48:54 +0530

    some solved

---
+++ a1367.cpp
@@ -0,0 +1,71 @@
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define fr(i, a, b) for (ll i = a; i < b; i++)
+#define _fr(i, a, b) for (ll i = a; i > b; i--)
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <queue> // also has priority queue
+#include <deque>
+#include <iomanip> // required for setprecision
+#include <stack>
+#include <numeric>
+#include <string>
+#include <limits.h>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int gcd(int a,int b)
+{
+    if(b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+int lcm(int a,int b)
+{
+    return a * b / gcd(a, b);
+}
+
+void solve()
+{
+    string s, ans; cin >> s;
+    ans.PB(s[0]);
+    ll n = s.size();
+    fr (i, 1, n - 1)
+    {
+        ans.PB(s[i]);
+        ++i;
+    }
+    ans.PB(s[n - 1]);
+    cout << ans << '\n';
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+}
