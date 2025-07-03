31b9490c63e9296678c77187b19eb303b2063e17
VedeshP <vedeshskp@gmail.com>
2025-07-03 12:35:08 +0530

    some solved acd

---
+++ a1935.cpp
@@ -0,0 +1,72 @@
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define fr(i, a, b) for (ll i = a; i < b; i++)
+#define _fr(i, a, b) for (ll i = a; i > b; i--)
+
+#define out_NO cout << "NO" << '\n';
+#define out_YES cout << "YES" << '\n';
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <queue> // also has priority queue
+#include <deque>
+#include <iomanip> // required for setprecision
+#include <stack>
+#include <numeric>
+#include <string>
+#include <limits.h>
+#include <cmath>
+#include <cctype>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int gcd(int a,int b)
+{
+    if(b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+int lcm(int a,int b)
+{
+    return a * b / gcd(a, b);
+}
+
+void solve()
+{
+    // doing more does help with learning more - whether it be more challenging or less challenging
+    // the sweet spot helps me get into the flow 
+    ll n; cin >> n;
+    string s; cin >> s;
+    string t = s;
+    reverse(t.begin(), t.end());
+    cout << min(s, t + s) << '\n';
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+}
